d37a81ecfa15cb7f54b0227b5c57e01e982a63e5
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-08-12 17:20:33 +0100

    commentaire

--- src/observerPattern/InterfaceObserver.ts
+++ src/observerPattern/InterfaceObserver.ts
@@ -1,4 +1,10 @@
 import {Observable} from './InterfaceObservable'
 export interface Observer{
-    update(observable:Observable):void
+
+    //il ya deux modéle push et pull
+    update(observable:Observable):void //pull
+    
+    //le model push permet de transfere etat de observer
+    //update(etat:Etat):void //pull
+    //Etat constien plus d'information dans notre cas etet représent seulemnt une number
 }
