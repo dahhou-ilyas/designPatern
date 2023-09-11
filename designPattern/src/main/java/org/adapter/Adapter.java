66e361308a036bd93f2752f533c7f2a8e6214c60
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-09-11 23:20:25 +0100

    finish

--- designPattern/src/main/java/org/adapter/Adapter.java
+++ designPattern/src/main/java/org/adapter/Adapter.java
@@ -2,7 +2,7 @@ package org.adapter;
 
 //si vous souhaiter d'utiliser approche par héritage changela déclaration
 //public class Adapter extends AncienneImpl implements Standard !!!!
-//l'approche par héritage il a un inconvénient (si on une class Abstait Standard au lieu d'un interface car le java ne support pas héritage multiple)
+//l'approche par héritage il a un inconvénient (si on une class Abstait Standard au lieu d'un interface car le java ne support pas héritage multiple "la class Adapter ne peut pas hériter de deux class")
 public class Adapter implements Standard{
     private AncienneImpl ancienne=new AncienneImpl();
     @Override
